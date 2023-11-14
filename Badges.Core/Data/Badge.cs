722623db59a16f1ca61238787c56d9991c427244
Rania <Rania@RANIA-PC>
2023-11-15 00:49:39 +0300

    Badges

--- Badges.Core/Data/Badge.cs
+++ Badges.Core/Data/Badge.cs
@@ -9,12 +9,6 @@ namespace Badges.Core.Data
         public string? Type { get; set; }
         public string? Text { get; set; }
         public string? Image { get; set; }
-        public decimal? FkAssignments { get; set; }
-        public decimal? FkUserid { get; set; }
-        public decimal? FkCourseid { get; set; }
 
-        public virtual Assignment? FkAssignmentsNavigation { get; set; }
-        public virtual Course? FkCourse { get; set; }
-        public virtual User? FkUser { get; set; }
     }
 }
